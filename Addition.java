b2a311d2696a2c1a95ac59fdb3bc6ef2a8bf0fcb
GyanveerSanidhya <117913637+GyanveerSanidhya@users.noreply.github.com>
2022-11-17 16:33:25 +0530

    Add files via upload

---
+++ Addition.java
@@ -0,0 +1,25 @@
+import java.util.*; 
+class Addition
+{ 
+  
+  public static void main(String[]args)
+   
+   {
+  
+     Scanner sc= new Scanner(System.in);
+ 
+       System.out.println("enter then first no.");
+  
+           int a=sc.nextInt();
+   
+       System.out.println("enter the second no.");
+   
+           int b=sc.nextInt();
+   
+           int c=a+b;
+    
+        System.out.println("output is"+c);
+
+   }
+
+}
---
+++ info.java
@@ -0,0 +1,25 @@
+import java.util.*; 
+class info
+{ 
+  
+  public static void main(String[]args)
+   
+   {
+  
+     Scanner sc= new Scanner(System.in);
+ 
+       System.out.println("enter your name");
+  
+           String a=sc.nextLine();
+   
+       System.out.println("enter your age");
+   
+           String b=sc.nextLine();
+    
+       System.out.println("Name-"+a);
+       System.out.println("Age-"+b);
+     
+
+   }
+
+}
