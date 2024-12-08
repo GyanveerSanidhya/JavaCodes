be5218f03040326b93bb389bc741557a222d7cdb
Nyctophile_24 <117913637+GyanveerSanidhya@users.noreply.github.com>
2024-12-08 12:03:14 +0530

    Add files via upload

---
+++ Bank.java
@@ -0,0 +1,9 @@
+public class Bank
+ {
+   public static void main(String[]args)
+      {
+        BankTransaction bankTransaction=new BankTransaction();
+        bankTransaction.withdrawal();
+        bankTransaction.deposit();
+      }
+ }
